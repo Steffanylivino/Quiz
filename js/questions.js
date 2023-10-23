8178c25cb96eeced33b4239f19cfcd71ad002378
Steffany Lima Oliveira Livino <steffany.livino@GRUPOOK.LOCAL>
2023-10-23 10:02:12 -0300

    init

---
+++ js/questions.js
@@ -0,0 +1,42 @@
+let questions =[
+    {
+        numb:1,
+        question:"Em qual país foi criado o primeiro celular?",
+        answer:"A.Estados Unidos",
+        options:["A.Estados Unidos", "B.China", "C.Inglaterra","D.França"
+
+        ]
+    },
+    {
+        numb:2,
+        question:"A marca que lançou o primeiro celular foi?",
+        answer:"C.Motorola",
+        options:["A.Samsung", "B.Xiaomi", "C.Motorola","D.Apple"
+            
+        ]
+    },
+    {
+        numb:3,
+        question:"Quanto tempo a primeira câmera do mundo levou para tirar uma foto?",
+        answer:"B.8 horas",
+        options:["A.12 horas ", "B.8 horas", "C.10 horas"," D.30 minutos"
+            
+        ]
+    },
+    {
+        numb:4,
+        question:"Qual foi o primeiro jogo de videogame inventado?",
+        answer:"B.Tennis for two",
+        options:["A.Pong ", "B.Tennis for two", "C.Space wars","D.Tetris"
+            
+        ]
+    },
+    {
+        numb:5,
+        question:"Em que ano foi criada a internet?",
+        answer:"B.1835",
+        options:["A.1989 ", "B.1835", "C.1963","D.1974"
+            
+        ]
+    }
+]
